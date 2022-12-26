c8d6171951925688621c674f622c8434909a54d0
andreas sujono <andr0075@e.ntu.edu.sg>
2022-12-26 12:49:59 +0800

    init app

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/blogs': { page: '/blogs' },
+    }
+  },
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
 }
 
